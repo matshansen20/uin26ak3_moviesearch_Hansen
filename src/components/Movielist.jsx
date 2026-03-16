0621d7b5c5bc7d288dcf4aa1937696e93753ee48
matshansen20 <matsah@hiof.no>
2026-03-16 13:41:06 +0100

    Update Movielist.jsx

---
+++ src/components/Movielist.jsx
@@ -0,0 +1,11 @@
+import MovieCard from "./MovieCard"
+
+export default function MovieList({ movies }) {
+  return (
+    <section>
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} />
+      ))}
+    </section>
+  )
+}
